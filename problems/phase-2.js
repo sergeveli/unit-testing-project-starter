1b3d67230a493a5c33f565f10c7edd8712e3e566
Antonio Davila-Olivares <ajd57@nau.edu>
2021-05-13 16:01:10 -0700

    working on phase 2

--- problems/phase-2.js
+++ problems/phase-2.js
@@ -16,7 +16,7 @@ const mirrorArray = (array) => {
     newArray.push(el);
   }
 
-  for (let i = array.length - 1; i >= 0; i += 1) {
+  for (let i = array.length - 1; i >= 0; i -= 1) {
     let el = array[i];
     newArray.push(el);
   }
@@ -24,21 +24,37 @@ const mirrorArray = (array) => {
   return newArray;
 };
 
+// const exampleArray2 = ['a', 'b', 'c']
+// console.log(mirrorArray(exampleArray2)) //>> ['a', 'b', 'c', 'c', 'b', 'a']
+
+
 /*
 2.  hiddenCounter
 Write a function named `hiddenCounter()`. This function will do two things - first
-it will define a count variable, then it will return a function. 
+it will define a count variable, then it will return a function.
 When invoked the function returned by hiddenCounter will increment the counter by 1.
 */
+
 function hiddenCounter() {
-  let count = undefined;
+  let count = 0;
   return () => (count += 1);
 }
+
+// let hiddenCounter = function () {
+//   let count = 0;
+//   return function () {
+//     count++;
+//     return count
+//   }
+// }
+
+// console.log(hiddenCounter())
+
 /*
-3. myMap 
+3. myMap
 Write a function  named `myMap` that accepts an array
 and a callback as arguments. The function will return an array of new elements
-obtained by calling the callback on each element of the array, passing in the 
+obtained by calling the callback on each element of the array, passing in the
 element. Assign the below function to a variable using the const keyword.
 
 Do not use the built in Array#map - use Array#forEach for iteration.
@@ -46,7 +62,7 @@ Do not use the built in Array#map - use Array#forEach for iteration.
 const myMap = (array, cb) => {
   let mapped = [];
 
-  array.forEach((el) => mapped.push(el));
+  array.forEach((el) => mapped.push(cb(el)));
   return mapped;
 };
 
--- test/phase-2-spec.js
+++ test/phase-2-spec.js
@@ -9,24 +9,27 @@ const { mirrorArray, hiddenCounter, myMap, avgValue } = require('../problems/pha
 describe('mirrorArray()', function () {
   it('should return a single array where the first half is the orginal array and the second half is a mirror of the first half', function () {
     //Arrange
-
+    let expected = ['a', 'b', 'c', 'c', 'b', 'a']
     //Act
-    
+    let actual = mirrorArray(['a', 'b', 'c'])
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(actual).to.eql(expected);
+
   })
 })
 
 //2. hiddenCounter
 describe('hiddenCounter()', function () {
-  
+
   it('should return a function that will increment the counter when invoked', function () {
     //Arrange
-
+    let expected = 2
     //Act
-
+    let actual = hiddenCounter()
+    actual()
     //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    expect(actual()).to.equal(expected)
+
   })
 })
 
@@ -34,16 +37,16 @@ describe('hiddenCounter()', function () {
 describe('myMap', function () {
  it("should function like the built in Array#map", function () {
     //Arrange
-   
+    let expected = [2, 4, 6, 8];
     //Act
-   
+    let actual = myMap([1, 2, 3, 4], (num) =>  num*2)
     //Assert
-   expect.fail('Remove this expect.fail and replace it with your test');
+    expect(actual).to.eql(expected)
   });
 
   it("should not call the built in Array#map", function () {
      //Arrange
-    
+
     //Act
 
     //Assert
@@ -55,7 +58,7 @@ describe('myMap', function () {
 describe('avgValue', function () {
   it('should return the average of an array of numbers', function () {
      //Arrange
-    
+
     //Act
 
     //Assert
